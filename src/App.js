bd5f08cef05f7e2ea729ada4d2c8304d13cdae50
Varun <varun.mupparaju007@gmail.com>
2024-01-13 20:54:07 +0530

    This is commited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <p>My name is Mupparaju Venkata Sai Varun.I am a second year Undergraduate student of the department of Computer science and engineering.I am from Ongole,Andhra Pradesh.I am a border of Radhakrishnan Hall of Residence.</p>
     </div>
   );
 }
